8b8cfef1100c02ff5902c9f96fcda756174f546b
rizkykojek <rizki.maulana@happyfresh.com>
2016-03-14 14:15:27 +0700

    Update and Edit Article model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
 
@@ -16,6 +20,18 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+
 
   def show
     @article = Article.find(params[:id])
