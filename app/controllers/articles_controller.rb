a622625987bf1fa349d72a4a0a66d7ed5323ad5d
rizkykojek <rizki.maulana@happyfresh.com>
2016-03-14 14:47:04 +0700

    Deleting Article model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -31,10 +31,15 @@ class ArticlesController < ApplicationController
     end
   end
 
-
-
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
