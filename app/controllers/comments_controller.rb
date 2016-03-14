c2be0d3bd1116ee92e23a580d2095a44181e6934
rizkykojek <rizki.maulana@happyfresh.com>
2016-03-14 16:08:53 +0700

    Deleting Comment from Article model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,16 @@ class CommentsController < ApplicationController
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
 
-    redirect_to articles_path(@article)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    #@comment = Comment.find(params[:id]) //more efficient finding code in below
+    @comment = @article.comments.find(params[:id])
+
+    @comment.destroy
+    redirect_to article_path(@article)
   end
 
   private
